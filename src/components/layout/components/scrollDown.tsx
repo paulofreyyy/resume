7e469730e4eb4bdae833dff991e6eca4d1a7980e
Paulo Henrique PErei <74430831+paulofreyyy@users.noreply.github.com>
2025-02-15 12:29:44 -0300

    style: criando animação de scrollDown

--- src/components/layout/components/scrollDown.tsx
+++ src/components/layout/components/scrollDown.tsx
@@ -1,9 +1,18 @@
 import { Box, Typography } from "@mui/material"
 import { FaArrowDown } from "react-icons/fa"
+import { motion } from "framer-motion"
 
 export const ScrollDown = () => {
     return (
         <Box
+            component={motion.div}
+            initial={{ y: -50, opacity: 0 }}
+            whileInView={{
+                y: 0,
+                opacity: 1,
+                transition: { duration: 0.8, ease: "easeOut" }
+            }}
+            viewport={{ once: true }}
             sx={{
                 position: 'fixed',
                 bottom: 20,
