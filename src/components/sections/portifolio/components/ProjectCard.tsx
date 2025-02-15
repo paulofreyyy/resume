70435a9059beae210751f0b4bd084f543230d622
Paulo Henrique PErei <74430831+paulofreyyy@users.noreply.github.com>
2025-02-15 11:27:52 -0300

    feat: finalizando seção Meus Trabalhos

--- src/components/sections/portifolio/components/ProjectCard.tsx
+++ src/components/sections/portifolio/components/ProjectCard.tsx
@@ -1,9 +1,22 @@
 import { Box, Typography } from "@mui/material"
+import { motion } from "framer-motion";
 
-export const ProjectCard = () => {
+interface Props {
+    link: string;
+    image: string;
+}
+
+export const ProjectCard = ({ link, image }: Props) => {
     return (
-        <>
+        <motion.div
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+            transition={{ duration: 0.5 }}
+        >
             <Box
+                component='a'
+                href={link}
+                target="_blank"
                 position='relative'
                 bgcolor='#3d3d3d'
                 borderRadius={5}
@@ -15,30 +28,36 @@ export const ProjectCard = () => {
                 justifyContent='center'
                 sx={{
                     cursor: 'pointer',
-                    backgroundImage: 'url(/projetos/lawyer.png)',
+                    backgroundImage: `url(/projetos/${image}.png)`,
                     backgroundPosition: 'center',
-                    backgroundSize: 'cover',
+                    backgroundSize: 'cover, contain',
+                    textDecoration: 'none',
                     '&: hover': {
                         '& .typography-hover': {
                             bgcolor: '#000',
-                            display:'block',
+                            display: 'block',
                         }
                     }
                 }}
             >
                 <Typography
                     className="typography-hover"
+                    component='a'
+                    href="https://github.com/paulofreyyy"
+                    target="_blank"
                     sx={{
                         fontWeight: 600,
+                        color: "#FFF",
+                        textDecoration: 'none',
                         width: 150,
                         py: 2,
                         borderRadius: 20,
-                        display:'none',
+                        display: 'none',
                     }}
                 >
                     Visualizar
                 </Typography>
             </Box>
-        </>
+        </motion.div >
     )
 }
--- src/components/sections/portifolio/index.tsx
+++ src/components/sections/portifolio/index.tsx
@@ -1,6 +1,16 @@
 import { Box, Typography } from "@mui/material"
 import Grid from '@mui/material/Grid2'
 import { ProjectCard } from "./components/ProjectCard"
+import { motion } from "framer-motion";
+
+const projetos = [
+    { link: "https://github.com/paulofreyyy/lawyer", image: 'lawyer' },
+    { link: "https://github.com/paulofreyyy/resume", image: 'resume' },
+    { link: "https://github.com/paulofreyyy/pokedex", image: 'pokedex' },
+    { link: "https://github.com/paulofreyyy/userDashboard", image: 'userDashboard' },
+    { link: "https://github.com/paulofreyyy/reading-manager", image: 'readingBook' },
+    { link: "https://kingbeneficios.org/", image: 'king' },
+]
 
 export const Portifolio = () => {
     return (
@@ -17,33 +27,42 @@ export const Portifolio = () => {
             <Typography variant="h3" fontWeight={600}>MEUS TRABALHOS</Typography>
 
             <Grid container spacing={6}>
-                <Grid size={4}>
-                    <ProjectCard />
-                </Grid>
+                {projetos.map((projeto, index) => (
+                    <Grid size={4} key={index}>
+                        <ProjectCard link={projeto.link} image={projeto.image} />
+                    </Grid>
+                ))}
             </Grid>
 
             <Box alignSelf='center'>
-                <Typography
-                    component="a"
-                    href="https://github.com/paulofreyyy"
-                    target="_blank"
-                    bgcolor='#FFF'
-                    color="#000"
-                    py={2}
-                    px={5}
-                    fontWeight={600}
-                    borderRadius={3}
-                    sx={{
-                        textDecoration:'none',
-                        transition: '0.7s ease',
-                        '&: hover':{
-                            bgcolor:'red',
-                            px: 7
-                        }
+                <motion.div
+                    initial={{ y: 50, opacity: 0, scale: 0.9 }}
+                    whileInView={{ y: 0, opacity: 1, scale: 1 }}
+                    viewport={{ once: true }}
+                    transition={{
+                        duration: 0.8,
+                        ease: "easeOut",
                     }}
+                    whileHover={{ scale: 1.2 }}
+                    whileTap={{ scale: 0.8 }}
                 >
-                    VER MAIS
-                </Typography>
+                    <Typography
+                        component="a"
+                        href="https://github.com/paulofreyyy"
+                        target="_blank"
+                        bgcolor='#FFF'
+                        color="#000"
+                        py={2}
+                        px={5}
+                        fontWeight={600}
+                        borderRadius={3}
+                        sx={{
+                            textDecoration: 'none',
+                        }}
+                    >
+                        VER MAIS
+                    </Typography>
+                </motion.div>
             </Box>
         </Box>
     )
